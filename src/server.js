639af7bc7a88348e9debec0427878bb3b2843d07
Kuba Żeligowski <k.zeligowski@gmail.com>
2021-10-30 01:26:10 -0400

    Static server done

--- src/server.js
+++ src/server.js
@@ -4,3 +4,8 @@ import dotenv from 'dotenv'
 
 // Get .env in as process.env
 dotenv.config()
+
+// Setup http static server
+const httpServer = express()
+httpServer.use('/', express.static('public'))
+httpServer.listen(process.env.HTTP_PORT, () => console.log(`HTTP Server started on port ${process.env.HTTP_PORT}`))
